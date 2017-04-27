707a80b02555cb0d28d46f31c9b8f1711b5ab45d
Ajinkya Dhaigude <ad8454@rit.edu>
2017-04-27 15:17:47 -0400

    create class for Object format

---
+++ info_symbol_object.py
@@ -0,0 +1,26 @@
+"""
+Class file to store information about a symbol
+represented as an Object.
+
+@author: Ajinkya Dhaigude (ad8454@rit.edu)
+@author: Supriya Godge (spg5835@rit.edu)
+"""
+
+
+class SymbolObject:
+    """
+    Class to contain information about each symbol Object
+    """
+
+    def __init__(self, trace_ids):
+        self.object_id = None
+        self.label = None
+        self.weight = None
+        self.trace_ids = trace_ids  # should be a list
+
+    def set_details(self, object_id, label, weight):
+        self.object_id = object_id
+        self.label = label
+        self.weight = str(weight)
+
+
