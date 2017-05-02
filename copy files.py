2f7cc1924fad769cb6c040da0fdbd2cee19aef0f
sup33 <spg5835@rit.edu>
2017-05-02 15:51:45 -0400

    Added new file copy_file

---
+++ copy files.py
@@ -0,0 +1,21 @@
+
+import os
+import shutil
+
+'''
+path="E:/PaternRec/Project2/GT_LG_Paridhi/"
+for fileName in os.listdir("E:/PaternRec/Project2/test_out"):
+        try:
+             w= open(path + fileName)
+             shutil.copy2(path + fileName, "E:/PaternRec/Project2/GT_testing")
+        except:
+                print("File not found",fileName)
+'''
+fileName="TraningFile.txt"
+
+with open(fileName) as fD:
+    for line in fD:
+        line = line.strip()
+        shutil.copy2(line, "E:/PaternRec/Project2/TrainFile")
+
+
